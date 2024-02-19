c230a8d619711841d4cd9f701f0b812fa21f9ced
eyob721 <eyobmelkamu721@gmail.com>
2024-02-19 11:42:09 +0300

    feat: enable all CSS features

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,3 +1,12 @@
 return {
   "norcalli/nvim-colorizer.lua",
+  opts = {
+    css = { -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      rgb_fn = true,
+      hsl_fn = true,
+      names = true,
+      RGB = true,
+      RRGGBB = true,
+    },
+  },
 }
