826d84c564132adfa0eef875df7da3e7b9165114
eyob721 <eyobmelkamu721@gmail.com>
2024-04-19 18:40:41 +0300

    feat: enable eslint LazyVim extra features

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,10 +12,11 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
     --[[ import any extras modules here ]]
-    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.clangd" }, -- Clangd
+    { import = "lazyvim.plugins.extras.lang.java" }, -- Java
+    { import = "lazyvim.plugins.extras.dap.core" }, -- Debugger
+    { import = "lazyvim.plugins.extras.linting.eslint" }, -- JS linter
     -- { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
