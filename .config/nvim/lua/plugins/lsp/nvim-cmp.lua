9c45009a08fe61eaa4464917e82af6b2cebdf37c
eyob721 <eyobmelkamu721@gmail.com>
2024-06-04 21:12:12 +0300

    feat: enable luasnip

--- .config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -67,15 +67,14 @@ return {
         end,
       }),
 
-      -- NOTE: nvim-snippet is taking care of this part
-      -- snippet = {
-      --   expand = function(args)
-      --     require("luasnip").lsp_expand(args.body)
-      --   end,
-      -- },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        -- { name = "luasnip" },
+        { name = "luasnip" },
         { name = "path" },
       }, {
         { name = "buffer" },
