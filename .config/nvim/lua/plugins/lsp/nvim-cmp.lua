5130f0ec527f82dc53aabbd5320849557414cc9a
eyob721 <eyobmelkamu721@gmail.com>
2024-06-04 13:49:37 +0300

    feat: trying out LazyVim CR and S-CR key behaviors

--- .config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -50,11 +50,17 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<S-CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+        -- My CR and S-CR
+        -- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ["<S-CR>"] = cmp.mapping.confirm({
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = true,
+        -- }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+        -- LazyVim CR and S-CR
+        ["<CR>"] = LazyVim.cmp.confirm(),
+        ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<C-CR>"] = function(fallback)
           cmp.abort()
           fallback()
