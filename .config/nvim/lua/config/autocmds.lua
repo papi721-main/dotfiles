6a0f428f05a3aeb07c58c066f7fafc3ca3458fca
eyob721 <eyobmelkamu721@gmail.com>
2024-07-23 09:42:16 +0300

    chore: add Java to the tabs filetypes

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,13 +7,25 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("papi_" .. name, { clear = true })
 end
 
+-- 4 tabs filetypes with tabs
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   group = augroup("c_files"),
-  pattern = { "*.c", "*.h" },
+  pattern = { "*.c", "*.h", "*.java" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
     vim.opt_local.expandtab = false
+  end,
+})
+
+-- 2 tab filetypes with spaces
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  group = augroup("small_indent_files"),
+  pattern = { "*.lua", "*.md", "*.html", "*.css", "*.mjs", "*.js", "*.sql", "*.json" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
   end,
 })
 
@@ -34,17 +46,6 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 --     vim.bo.filetype = "sh"
 --   end,
 -- })
-
--- 2 tab filetypes
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  group = augroup("small_indent_files"),
-  pattern = { "*.lua", "*.md", "*.html", "*.css", "*.mjs", "*.js", "*.sql", "*.json" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.expandtab = true
-  end,
-})
 
 -- TODO: Remeber to enable autocmd for JS linter
 -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
