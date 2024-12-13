ebbdc7b2ca6436053c64fcf53e65e25e72e1002b
papi72112 <eyobmelkamu721@gmail.com>
2024-12-13 15:30:02 +0300

    feat: time to start my neovim config from sractch, agian ( ... it has been a long time )

--- .config/nvim/lua/plugins/dap/mason-nvim-dap.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "jay-babu/mason-nvim-dap.nvim",
-  opts = {
-    ensure_installed = {
-      "cppdbg", -- C and C++
-      "python", -- Python
-    },
-  },
-}
--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "echasnovski/mini.comment", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-}
