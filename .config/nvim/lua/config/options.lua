6b51a8b3d2396c413b6eaabcdcdf90a720fa2d60
papi72112 <99392252+papi72112@users.noreply.github.com>
2025-03-14 08:02:40 +0300

    feat (nvim): turn off tabs and indentations options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,12 +5,12 @@
 local opt = vim.opt
 
 -- [[ Tabs / Indentation ]]
-opt.tabstop = 4 -- Number of spaces a <Tab> counts for
-opt.shiftwidth = 0 -- Number of spaces to use for each space of autoindent
-opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
-opt.expandtab = true -- Expand tabs to spaces
-opt.shiftround = true -- Rounds indent to multiple of `shiftwidth`, applies to > and <
-opt.smartindent = true -- automatic indenting
+-- opt.tabstop = 4 -- Number of spaces a <Tab> counts for
+-- opt.shiftwidth = 0 -- Number of spaces to use for each space of autoindent
+-- opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
+-- opt.expandtab = true -- Expand tabs to spaces
+-- opt.shiftround = true -- Rounds indent to multiple of `shiftwidth`, applies to > and <
+-- opt.smartindent = true -- automatic indenting
 
 -- [[ Formatting ]]
 opt.textwidth = 79
